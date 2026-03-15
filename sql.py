c095f5ce24ab852efa4850cf8e40918543c0096d
Muhammet Emirhan <158741458+memirhan@users.noreply.github.com>
2026-03-15 12:27:48 +0300

    Create sql.py

---
+++ sql.py
@@ -0,0 +1,22 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "testdb");
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    echo "Login başarılı";
+} else {
+    echo "Login başarısız";
+}
+?>
+
+<form method="POST">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <button type="submit">Login</button>
+</form>
