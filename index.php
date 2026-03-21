62060ba9855d62fa4a4c4b5a7248e314a909a3e3
Muhammet Emirhan <158741458+memirhan@users.noreply.github.com>
2026-03-22 01:14:29 +0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+username = input("Username: ")
+password = input("Password: ")
+
+cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+
+user = cursor.fetchone()
+
+if user:
+    print("Login successful")
+else:
+    print("Invalid credentials")
