d73d64d2b38333b761fca946b0f409860dcd99eb
Muhammet Emirhan <158741458+memirhan@users.noreply.github.com>
2026-04-06 13:03:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-import sqlite3
-import bcrypt
+<?php
+// Kullanıcıdan veri al
+$input = $_GET['q'];
 
-conn = sqlite3.connect("users.db")
-cursor = conn.cursor()
-
-username = input("Username: ")
-password = input("Password: ")
-
-cursor.execute("SELECT password_hash FROM users WHERE username=?", (username,))
-result = cursor.fetchone()
-
-if result:
-    stored_hashed_password = result[0].encode('utf-8')
-    if bcrypt.checkpw(password.encode('utf-8'), stored_hashed_password):
-        print("Login successful")
-    else:
-        print("Invalid credentials")
-else:
-    print("Invalid credentials")
+// Direkt ekrana bas (XSS açığı!)
+echo "Arama sonucu: " . $input;
+?>
